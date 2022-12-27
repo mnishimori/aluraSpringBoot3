38d9cd3eaaa82f112e462d5f1e24dcf9fc7434b4
mnishimori <mnishimori@yahoo.com.br>
2022-12-27 13:02:07 -0300

    feat: Classe para tratamento de erros em escopo de API.

---
+++ src/main/java/med/voll/api/infrastructure/ApiExceptionHandler.java
@@ -0,0 +1,40 @@
+package med.voll.api.infrastructure;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handler404Error(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handler400Error(MethodArgumentNotValidException exception){
+
+        var errors = exception.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(errors.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity handlerSQLIntegrityConstraintViolationException(
+            SQLIntegrityConstraintViolationException exception){
+
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    private record DadosErroValidacao(String campo, String message){
+        public DadosErroValidacao(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
