11f56475e48eec8a0359c7b451386235e06a9165
mnishimori <mnishimori@yahoo.com.br>
2022-12-28 13:42:33 -0300

    refactor: atualização de import de repository

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,7 +3,7 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.domain.endereco.Endereco;
 import med.voll.api.domain.medico.*;
-import med.voll.api.domain.repository.MedicoRepository;
+import med.voll.api.domain.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
