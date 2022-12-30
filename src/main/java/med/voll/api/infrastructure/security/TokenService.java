0431f70b5cabfafb4ba17897082fd90e1c7dc321
mnishimori <mnishimori@yahoo.com.br>
2022-12-30 20:06:17 -0300

    feat: classe para para validar o token

--- src/main/java/med/voll/api/infrastructure/security/TokenService.java
+++ src/main/java/med/voll/api/infrastructure/security/TokenService.java
@@ -3,7 +3,9 @@ package med.voll.api.infrastructure.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -13,22 +15,38 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
+    @Value("${api.security.token.secret}")
+    private String secret;
+
     public String gerarToken(Usuario usuario){
         try {
-            Algorithm algorithm = Algorithm.HMAC256("123456");
+            Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API voll.med")
                     .withSubject(usuario.getLogin())
-                    .withClaim("id", usuario.getId())
                     .withExpiresAt(getDataExpiracao())
                     .sign(algorithm);
         } catch (JWTCreationException exception){
-            // Invalid Signing configuration / Couldn't convert Claims.
             throw new RuntimeException("Erro ao gerar o token JWT");
         }
     }
 
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(this.secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            exception.printStackTrace();
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+    }
+
     private Instant getDataExpiracao() {
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+        var dataExpiracao = LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+        return dataExpiracao;
     }
 }
