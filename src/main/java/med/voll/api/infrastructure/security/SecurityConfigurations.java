84e79b57bb8a1bf39204aa32453439936394671b
mnishimori <mnishimori@yahoo.com.br>
2023-03-12 16:58:36 -0300

    feat(consulta): configurações do spring doc.

--- src/main/java/med/voll/api/infrastructure/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infrastructure/security/SecurityConfigurations.java
@@ -28,6 +28,7 @@ public class SecurityConfigurations {
         .and()
         .authorizeHttpRequests()
         .requestMatchers(HttpMethod.POST, "/login").permitAll()
+        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
         .anyRequest().authenticated()
         .and()
         .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
